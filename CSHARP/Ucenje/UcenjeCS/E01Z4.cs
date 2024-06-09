2431f5791a48962285ab649f3d1099f0c5a0a881
DamirBukvasevic <damirbukvasevic@gmail.com>
2024-06-09 19:04:45 +0200

    Zadatak E01Z4 Rjesenje

---
+++ CSHARP/Ucenje/UcenjeCS/E01Z4.cs
@@ -0,0 +1,26 @@
+﻿namespace UcenjeCS
+{
+    internal class E01Z4
+    {
+        public static void Izvedi()
+
+        {
+            // zadatak: Unijeti Adresu i grad te ispisati jedno ispod drugog adresu i grad
+
+            string Adresa;
+            string Grad;
+
+            Console.Write("Unesite adresu: ");
+            Adresa = Console.ReadLine();
+
+            Console.Write("Unesite grad: ");
+            Grad = Console.ReadLine();
+
+            Console.WriteLine("Rezultat: ");
+            Console.WriteLine(Adresa);
+            Console.WriteLine(Grad);
+
+        }
+
+    }
+}

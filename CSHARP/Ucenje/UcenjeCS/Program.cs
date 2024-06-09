1de8bcd59011b5ba9771304b4db3f006dfbbb1f3
DamirBukvasevic <damirbukvasevic@gmail.com>
2024-06-09 20:09:55 +0200

    Program Izvedi //

--- CSHARP/Ucenje/UcenjeCS/Program.cs
+++ CSHARP/Ucenje/UcenjeCS/Program.cs
@@ -1,12 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using UcenjeCS;
+
+//E01Z4.Izvedi();
+//E01Z3.Izvedi();
+//E01Z2.Izvedi();
+//E01Z1.Izvedi();
+//E02VarijableTipoviPodatakaOperatori.Izvedi();
+//E01UlazIzlaz.Izvedi();
+
+
+
+
+
+// See https://aka.ms/new-console-template for more information
 /*
  * 
  * Komentar kroz
  * više redova
  * 
  */
+/*
 Console.WriteLine("Hello, World!");
 Console.WriteLine(7);
 Console.WriteLine(true);
 
-Console.WriteLine("Damir");
+Console.WriteLine("Tomislav");
+
+*/
+
+

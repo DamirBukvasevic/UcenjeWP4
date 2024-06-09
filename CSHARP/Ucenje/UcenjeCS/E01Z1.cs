382f3693987413cbc5c21f92d9b1c6381bdd2cfd
DamirBukvasevic <damirbukvasevic@gmail.com>
2024-06-09 18:50:10 +0200

    Zadatak E01Z1 Rjesenje

---
+++ CSHARP/Ucenje/UcenjeCS/E01Z1.cs
@@ -0,0 +1,26 @@
+﻿namespace UcenjeCS
+{
+    internal class E01Z1
+    {
+
+        public static void Izvedi()
+        {
+            // zadatak
+            // Korisnik unosi dva cijela broja i ispisuje njihov zbroj
+
+            int broj1;
+            int broj2;
+
+            Console.Write("Unesite broj1: ");
+            broj1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Unesite broj2: ");
+            broj2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Rezultat: ");
+            Console.WriteLine(broj1 + broj2);
+
+        }
+
+    }
+}
